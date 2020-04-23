2de9ecbae5a0b4c172feb784d37098b64fc4a82b
jane.lamia <o.zhenya@gmail.com>
2020-04-23 13:21:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ParallelGraph
+{
+    
+    public class Node<T>
+    {
+        public T Val { get; private set; }
+        public readonly int Ind;
+
+        public Node(T val, int ind)
+        {
+            Val = val;
+            Ind = ind;
+        }
+    }
+    public class Graph<T>
+    {
+        private readonly List<Node<T>>[] _adjacencyList;
+        private readonly List<Node<T>>[] _parentsList;
+        private readonly Node<T>[] _allNodes;
+        public readonly int Size;
+
+        public Node<T> GetNode(int ind)
+        {
+            return _allNodes[ind];
+        }
+
+        public List<Node<T>> GetParents(Node<T> node)
+        {
+            return _parentsList[node.Ind];
+        }
+
+
+        public int GetParentsCount(Node<T> node)
+        {
+            return _parentsList[node.Ind].Count;
+        }
+
+        public int GetParentsCount(int ind)
+        {
+            return _parentsList[ind].Count;
+        }
+
+        public List<Node<T>> GetChildren(Node<T> node)
+        {
+            return _adjacencyList[node.Ind];
+        }
+
+
+        public Graph(int size)
+        {
+            Size = size;
+            _adjacencyList = new List<Node<T>>[Size];
+            _parentsList = new List<Node<T>>[Size];
+            _allNodes = new Node<T>[Size];
+        }
+
+        public Graph(int size, Node<T>[] nodes, List<Tuple<int, int>> edges)
+        {
+            Size = size;
+
+            _allNodes = new Node<T>[Size];
+            _parentsList = new List<Node<T>>[Size];
+            _adjacencyList = new List<Node<T>>[Size];
+
+            for (int i = 0; i < Size; i++)
+            {
+                _parentsList[i] = new List<Node<T>>();
+                _adjacencyList[i] = new List<Node<T>>();
+            }
+
+            _allNodes = nodes;
+
+            foreach (var edge in edges)
+            {
+                var v = edge.Item1;
+                var u = edge.Item2;
+                _adjacencyList[v].Add(_allNodes[u]);
+                _parentsList[u].Add(_allNodes[v]);
+            }
+        }
+    }
+
+    class Program
+    {
+        private static async Task ProcessInParallelAsync(Graph<Action> actions)
+        {
+            //var ts = new myTaskScheduler(1);
+            var factory = new TaskFactory();
+            var allTasks = new List<Task>();
+            var map = new Dictionary<Task, Node<Action>>();
+            var finishedActions = new bool[actions.Size];
+
+            for (var i = 0; i < actions.Size; i++)
+            {
+                if (actions.GetParentsCount(i) != 0) continue;
+                var task = factory.StartNew(actions.GetNode(i).Val);
+                allTasks.Add(task);
+               // var opts = task.CreationOptions;
+                map[task] = actions.GetNode(i);
+            }
+
+            while (allTasks.Any())
+            {
+                //Console.WriteLine("Come to tasks processing");
+                var finished = await Task.WhenAny(allTasks);
+               // Console.WriteLine("Smth has finished");
+                Console.WriteLine(allTasks.Count);
+                for (var i = 0; i < allTasks.Count; i++)
+                {
+                    var curTask = allTasks[i];
+                    if (finished != curTask) continue;
+                    var node = map[curTask];
+                    finishedActions[node.Ind] = true;
+                    foreach (var child in actions.GetChildren(node))
+                    {
+                        var canBeStarted = true;
+                        foreach (var parent in actions.GetParents(child)
+                            .Where(parent => !finishedActions[parent.Ind]))
+                        {
+                            canBeStarted = false;
+                        }
+
+                        if (!canBeStarted) continue;
+                        var newTask = factory.StartNew(child.Val);
+                        map[newTask] = child;
+                        allTasks.Add(newTask);
+                        Console.WriteLine("Task {0} is started", child.Ind + 1);
+                        //factory.StartNew(map[allTasks.Last()]);
+                    }
+                }
+                allTasks.Remove(finished);
+            }
+        }
+        public static async Task Main()
+        {
+            Action a1 = () => { Console.WriteLine("1"); };
+            Action a2 = () => { Console.WriteLine("2"); Thread.Sleep(200);};
+            Action a3 = () => { Console.WriteLine("3"); Thread.Sleep(300);};
+            var nodes = new Node<Action>[3];
+            nodes[0] = new Node<Action>(a1, 0);
+            nodes[1] = new Node<Action>(a2, 1);
+            nodes[2] = new Node<Action>(a3, 2);
+            List<Tuple<int, int>> lst = new List<Tuple<int, int>>();
+            lst.Add(new Tuple<int, int>(0, 1));
+            lst.Add(new Tuple<int, int>(0, 2));
+            var graph = new Graph<Action>(3, nodes, lst);
+            await ProcessInParallelAsync(graph);
+        }
+    }
+}
---
+++ TaskScheduler.cs
@@ -0,0 +1,202 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection; 
+
+
+namespace ParallelGraph
+{
+    public class MyTaskScheduler : TaskScheduler
+    {
+        private MyThreadPool _mThreadPool;
+        private static int _defaultNumOfThreads = Environment.ProcessorCount;
+        public new int Id { get; }
+
+        public MyTaskScheduler(int i, int numOfThreads, Exception argumentException)
+        {
+            _mThreadPool = new MyThreadPool(numOfThreads);
+            Id = i;
+            ArgumentException = argumentException;
+        }
+
+        public MyTaskScheduler(int i, Exception argumentException)
+        {
+            ArgumentException = argumentException;
+            _mThreadPool = new MyThreadPool(_defaultNumOfThreads);
+        }
+
+        protected override IEnumerable<Task>? GetScheduledTasks()
+        {
+            return _mThreadPool.GetWorkQueueTasks();
+        }
+
+        private static readonly ParameterizedThreadStart LongRunningThreadWork = s =>
+        {
+            Debug.Assert(s is Task);
+            var executeMethod = typeof(Task).GetMethod("ExecuteWithThreadLocal",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            //((Task)s).ExecuteEntryUnsafe(threadPoolThread: null);
+            executeMethod.Invoke((Task) s, new object[] {null});
+        };
+
+        protected override void QueueTask(Task task)
+        {
+            if (task == null)
+                throw ArgumentException;
+            var options = task.CreationOptions;
+            if ((options & TaskCreationOptions.LongRunning) != 0)
+            {
+                Thread thread = new Thread(LongRunningThreadWork);
+                thread.IsBackground = true;
+                thread.Start(task);
+            }
+            else
+            {
+               _mThreadPool.QueueUserTask(task);
+            }
+        }
+
+        public Exception ArgumentException { get ; set; }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MyThreadPool : IDisposable
+    {
+
+        private  Queue<Task> _workQueue;
+        private  List<Thread> _threads;
+        private int _numberofThreads;
+        private ManualResetEvent _stopEvent;
+        private object _stopLock;
+        private bool _isDisposed;
+        private Thread _scheduleThread;
+        private ManualResetEvent _scheduleEvent;
+        private Dictionary<int, ManualResetEvent> _threadsEvent;
+
+        public void QueueUserTask(Task task)
+        {
+            if (task == null)
+                throw new ArgumentException("Task can't be null!");
+            lock (_stopLock)
+                _workQueue.Enqueue(task);
+        }
+        
+        public MyThreadPool(int numberOfThreads)
+        {
+            if (numberOfThreads <= 0)
+                throw new ArgumentException("Number of threads must be positive");
+            
+            _numberofThreads = numberOfThreads;
+            _threadsEvent = new Dictionary<int, ManualResetEvent>();
+
+            _workQueue = new Queue<Task>();
+            _threads = new List<Thread>();
+            _stopLock = new object();
+            
+            _stopEvent = new ManualResetEvent(false);
+            _scheduleEvent = new ManualResetEvent(false);
+
+            _isDisposed = false;
+            
+            _scheduleThread = new Thread(FindAndStartFreeThread) {IsBackground = true};
+
+            for (int i = 0; i < _numberofThreads; i++)
+            {
+                var thread = new Thread(() => ThreadWork(i));
+                _threads.Add(thread);
+                _threads[i].Start();
+            }
+        }
+
+        private void FindAndStartFreeThread()
+        {
+            for (;;)
+            {
+                _scheduleEvent.WaitOne();
+                lock (_threads)
+                {
+                    foreach (var thread in _threads)
+                    {
+                        if (_threadsEvent[thread.ManagedThreadId].WaitOne() == false)
+                        {
+                            _threadsEvent[thread.ManagedThreadId].Set();
+                            break;
+                        }
+                    }
+                }
+
+                _scheduleEvent.Reset();
+            }
+        }
+        
+        private void ThreadWork(int i)
+        {
+            var lockObj = new object();
+            lock (lockObj)
+            {
+                while (true)
+                {
+                    if (_workQueue.Count == 0) continue;
+                    Console.WriteLine($"Thread number {i}, starts task");
+                    var task = _workQueue.Dequeue();
+                    if (task == null) continue;
+                    var threadCallMethod = typeof(Task).GetMethod("ExecuteWithThreadLocal",
+                        BindingFlags.Instance | BindingFlags.NonPublic);
+                    threadCallMethod.Invoke(this, new object[] {task, _threads[i]});
+                    //task.ExecuteFromThreadPool(threads[i]);
+                }
+            }
+        }
+
+        private void RemoveTask()
+        {
+            lock (_workQueue)
+                _workQueue.Dequeue();
+        }
+
+        public IEnumerable<Task>? GetWorkQueueTasks()
+        {
+            return _workQueue;
+        }
+
+        ~MyThreadPool()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+            _stopEvent.Dispose();
+            _scheduleEvent.Dispose();
+        }
+        
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    _scheduleThread.Abort();
+                    _scheduleEvent.Dispose();
+
+                    for (int i = 0; i < _numberofThreads; i++)
+                    {
+                        _threads[i].Abort();
+                        _threadsEvent[_threads[i].ManagedThreadId].Dispose();
+                    }
+                }
+                _isDisposed = true;
+            }
+        }
+    }
+}
